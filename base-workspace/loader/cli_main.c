0de8b7489e5002aa7c76eac31fac2012885becc7
Lie <37653418+lie@users.noreply.github.com>
2019-11-13 22:49:40 +0900

    仮にハッシュ確認機能を追加。コンパイルは通るがEV3実行時に落ちる。

--- base-workspace/loader/cli_main.c
+++ base-workspace/loader/cli_main.c
@@ -21,6 +21,8 @@
 #include "apploader.h"
 
 #include <stdlib.h>
+#include <string.h>
+#include "sha2_concat.h"
 
 static bool_t load_success;
 
@@ -95,10 +97,10 @@ static int compare_hash(STK_T* hash1, STK_T* hash2, uint32_t hash_size) {
 	uint32_t i = 0;
 	for(i = 0; i < hash_size; ++i) {
 		if(hash1[i] < hash2[i]) {
-			syslog("%lu %lu", hash1[i], hash2[i]);
+			syslog("%lo %lo", (unsigned long)(hash1[i]), (unsigned long)(hash2[i]));
 			return -1;
 		} else if(hash1[i] > hash2[i]) {
-			syslog("%lu %lu", hash1[i], hash2[i]);
+			syslog("%lo %lo", (unsigned long)(hash1[i]), (unsigned long)(hash2[i]));
 			return 1;
 		}
 	}
@@ -106,11 +108,18 @@ static int compare_hash(STK_T* hash1, STK_T* hash2, uint32_t hash_size) {
 	return 0;
 }
 
-static void calculate_hash(STK_T* hash, uint32_t hash_size) {
-	uint32_t i = 0;
-	for(i = 0; i < hash_size; ++i) {
-		hash[i] = 0x6867666564636261;
+static void calculate_hash(char* hash, uint32_t hash_size, char* plaintext, uint32_t plaintext_size) {
+	SHA256_CTX sha_ctx;
+	SHA256_Init(&sha_ctx);
+	SHA256_Update(&sha_ctx, plaintext, plaintext_size);
+	SHA256_Final(hash, &sha_ctx);
+
+	char log_message[SHA256_DIGEST_LENGTH * 2 + 1];
+
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+		sprintf(log_message + (i * 2), "%02x", hash[i] & 0xFF);
 	}
+	syslog(LOG_NOTICE, "SHA: %s\r", log_message);
 }
 
 static
@@ -344,10 +353,6 @@ void test_bluetooth_pan_loader_secure_update(intptr_t portid) {
     sprintf(msg_otp, "One-Time Password: %s", otp);
     show_message_box("Secure Update", msg_otp);
 
-    const uint32_t hash_size = 32;
-    STK_T received_hash[hash_size];
-    STK_T calculated_hash[hash_size];
-
 	/**
 	 * Draw GUI
 	 */
@@ -374,25 +379,42 @@ void test_bluetooth_pan_loader_secure_update(intptr_t portid) {
         if (http_receive_file_poll(&file_name, &file_size)) { // File received
             syslog(LOG_NOTICE, "File '%s' (%lu bytes) is received.", file_name, file_size);
             if (strcmp("", file_name) == 0) { // uImage received
-            	if(file_size < sizeof(STK_T) * hash_size) {
-            		// ファイルサイズがハッシュ長より小さいのはおかしい。処理もバグるので終了
+            	char   received_hash[SHA256_DIGEST_LENGTH];
+            	char calculated_hash[SHA256_DIGEST_LENGTH];
+
+            	// ファイルサイズがハッシュ長より小さいのはおかしい。処理もバグるので終了
+            	if(file_size < SHA256_DIGEST_LENGTH) {
             		show_message_box("Secure Update", "File size is too small.");
             		break;
+            	} else {
+            		show_message_box("Secure Update", "File size is valid.");
             	}
 
-            	uint32_t buf_ll_size = file_size / sizeof(STK_T);
-            	uint32_t hash_index = buf_ll_size - hash_size;
-            	for(hash_index = 0; hash_index < hash_size; ++hash_index) {
-            		received_hash[hash_index] = app_binary_buf[hash_index + buf_ll_size - hash_size];
+            	uint32_t    otp_size = 0;
+            	uint32_t   hash_size = SHA256_DIGEST_LENGTH;
+            	uint32_t binary_size = file_size - hash_size - otp_size;
+
+            	char* buf_char_pointer = (char *)app_binary_buf;
+            	for(uint32_t hash_index = 0; hash_index < hash_size; ++hash_index) {
+            		char formsg[32];
+            		sprintf(formsg, "hash[%lu] = %02x", hash_index, buf_char_pointer[hash_index])
+            		received_hash[hash_index] = buf_char_pointer[binary_size + hash_index];
             	}
 
-            	calculate_hash(calculated_hash, hash_size);
+            	calculate_hash(calculated_hash, hash_size, app_binary_buf, binary_size);
             	if(compare_hash(calculated_hash, received_hash, hash_size) != 0) {
             		show_message_box("Secure Update", "Hash value is invalid.");
             		break;
+            	} else {
+            		show_message_box("Secure Update", "Hash value is valid.");
             	}
 
-                apploader_store_file("/uImage", app_binary_buf, file_size - hash_size * sizeof(STK_T));
+            	/* ココまでは実行できる */
+            	char msg[32];
+            	sprintf(msg, "%lu, %lu, %lu, %lu", otp_size, hash_size, binary_size, file_size);
+            	show_message_box("Secure Update", msg);
+
+                apploader_store_file("/uImage", app_binary_buf, binary_size); // ハッシュ部分は無視
 	            show_message_box("uImage Received", "Please restart  EV3 to use it.");
             } else { // User application received
     	        static char filepath[1024/* TODO: check length? */];
@@ -414,11 +436,30 @@ void test_bluetooth_pan_loader_secure_update(intptr_t portid) {
 	show_message_box("Error", "Failed to load application.");
 }
 
+static void sha_test(intptr_t portid) {
+	char *message = {"Sample Message"};
+	unsigned char digest[SHA256_DIGEST_LENGTH];
+	
+	SHA256_CTX sha_ctx;
+	SHA256_Init(&sha_ctx); // コンテキストを初期化
+	SHA256_Update(&sha_ctx, message, strlen(message)); // message を入力にする
+	SHA256_Final(digest, &sha_ctx); // digest に出力
+
+	syslog(LOG_NOTICE, "message = %s\r", message);
+	
+	syslog(LOG_NOTICE, "SHA256: \r");
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+		syslog(LOG_NOTICE, "%02x", digest[i]);
+	}
+	syslog(LOG_NOTICE, "\r\n");
+}
+
 static const CliMenuEntry entry_tab[] = {
 	{ .key = '1', .title = "SD card", .handler = test_sd_loader },
 	{ .key = '2', .title = "Bluetooth PAN", .handler = test_bluetooth_pan_loader },
 	{ .key = '3', .title = "Bluetooth SPP", .handler = test_serial_loader, .exinf = SIO_PORT_BT },
 	{ .key = '4', .title = "Secure Update", .handler = test_bluetooth_pan_loader_secure_update },
+	{ .key = '5', .title = "SHA Test", .handler = sha_test },
 //	{ .key = '3', .title = "Serial port 1", .handler = test_serial_loader, .exinf = SIO_PORT_UART  },
 //	{ .key = 'D', .title = "Download Application", },
 	{ .key = 'Q', .title = "Exit to console", .handler = NULL },
